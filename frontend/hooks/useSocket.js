87b9258f0eced7084d93904ddc105fb1169ee8fc
ShakirYasin <shakir.yasin18@gmail.com>
2023-04-06 17:37:17 +0500

    URL Changes

--- frontend/hooks/useSocket.js
+++ frontend/hooks/useSocket.js
@@ -5,6 +5,7 @@ const useSocket = (endpoint = import.meta.env.VITE_FLY_ENDPOINT) => {
   const socket = useMemo(() => io(endpoint, {
     transports:  [ "websocket", "polling" ],
     port: "8080",
+    host: import.meta.env.VITE_FLY_ENDPOINT
   }), [endpoint]);
   return socket;
 };
