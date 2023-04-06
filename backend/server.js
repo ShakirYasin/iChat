886aaf6be0092c67be66b8dc51fa13ba3a822fee
ShakirYasin <shakir.yasin18@gmail.com>
2023-04-06 16:30:40 +0500

    Changes

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use('/api/message',  require('./routes/messageRoutes'))
 app.use(notFound)
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 
 const server = app.listen(PORT, console.log(`Server Started on PORT ${PORT}`.yellow.bold))
 
