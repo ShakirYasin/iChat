b4b4d5a064336d98dfb5413a47cee4691c9ff1a8
ShakirYasin <shakir.yasin18@gmail.com>
2023-04-06 17:35:01 +0500

    URL Changes

--- frontend/hooks/useSocket.js
+++ frontend/hooks/useSocket.js
@@ -5,7 +5,6 @@ const useSocket = (endpoint = import.meta.env.VITE_FLY_ENDPOINT) => {
   const socket = useMemo(() => io(endpoint, {
     transports:  [ "websocket", "polling" ],
     port: "8080",
-    
   }), [endpoint]);
   return socket;
 };
--- frontend/utils/axios.js
+++ frontend/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: import.meta.env.VITE_FLY_ENDPOINT,
+    baseURL: `${import.meta.env.VITE_FLY_ENDPOINT}/api`,
 })
    
 export default instance
